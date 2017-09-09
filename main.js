1722d092d5d3570ee1c9f1d26aec7e6d8be05bf2
Peter Richardson <peter@mapzen.com>
2017-09-08 18:41:28 -0700

    add scene url param

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
 /*jslint browser: true*/
 /*global Tangram, gui */
 
+function parseQuery (qstr) {
+    var query = {};
+    var a = qstr.split('&');
+    for (var i in a) {
+        var b = a[i].split('=');
+        query[decodeURIComponent(b[0])] = decodeURIComponent(b[1]);
+    }
+    return query;
+}
+
 map = (function () {
     'use strict';
 
@@ -18,6 +28,16 @@ map = (function () {
         map_start_location = map_start_location.map(Number);
     }
 
+    // determine the scene url and content to load during start-up
+    var scene_url = 'scene.yaml';
+
+    // If there is a query, use it as the scene_url
+    var query = parseQuery(window.location.search.slice(1));
+    if (query.url) {
+        console.log(query.url)
+        scene_url = query.url;
+    }
+
     /*** Map ***/
 
     var map = L.map('map',
@@ -25,7 +45,7 @@ map = (function () {
     );
 
     var layer = Tangram.leafletLayer({
-        scene: 'scene.yaml',
+        scene: scene_url,
         attribution: '<a href="https://mapzen.com/tangram" target="_blank">Tangram</a> | &copy; OSM contributors | <a href="https://mapzen.com/" target="_blank">Mapzen</a>'
     });
 
@@ -34,7 +54,8 @@ map = (function () {
     window.scene = scene;
 
     // setView expects format ([lat, long], zoom)
-    map.setView(map_start_location.slice(0, 3), map_start_location[2]);
+    console.log(map_start_location.slice(0, 2), map_start_location[2])
+    map.setView(map_start_location.slice(0, 2), map_start_location[2]);
 
     var hash = new L.Hash(map);
 
