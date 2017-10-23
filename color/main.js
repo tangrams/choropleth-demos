50ffd063af89aaf9eba7e1274029c98e38b7ea4a
Peter Richardson <peter@mapzen.com>
2017-10-23 16:50:40 -0700

    color adjustment example

--- color/main.js
+++ color/main.js
@@ -42,53 +42,26 @@ map = (function () {
     function addGUI () {
         gui.domElement.parentNode.style.zIndex = 1000; // make sure GUI is on top of map
         window.gui = gui;
-        gui.linear = true;
-        gui.add(gui, 'linear').listen().onChange(function() {
-            gui.linear = true;
-            gui.log = false;
-            scene.config.layers['states-linear'].enabled = true;
-            scene.config.layers['states-log'].enabled = false;
+        gui.color = "#ffae23"; // CSS string
+        gui.addColor(gui, 'color').onChange(function(value) {
+            // console.log(hexToRgb(value));
+            scene.config.global.color = value;
             scene.updateConfig();
         });
-        gui.log = false;
-        gui.add(gui, 'log').listen().onChange(function() {
-            gui.log = true;
-            gui.linear = false;
-            scene.config.layers['states-log'].enabled = true;
-            scene.config.layers['states-linear'].enabled = false;
+        gui.brightness = 0;
+        gui.add(gui, 'brightness', -10, 10).listen().onChange(function(value) {
+            scene.config.global.brightness = value;
             scene.updateConfig();
         });
-
-        gui.labels = false;
-        gui.add(gui, 'labels').onChange(function(value) {
-            scene.config.global.textvisible = value;
+        gui.add(gui, 'saturation').listen().onChange(function() {
+            scene.config.global.saturation = value;
+            scene.updateConfig();
+        });
+        gui.add(gui, 'divisions').listen().onChange(function(value) {
+            scene.config.global.divisions = value;
             scene.updateConfig();
         });
 
-        gui.lines = false;
-        gui.add(gui, 'lines').onChange(function(value) {
-            scene.config.global.linesvisible = value;
-            scene.updateConfig();
-        });
-
-        gui.minval = 100;
-        gui.add(gui, 'minval', 0, 10000).name("minimum value").onChange(function(value) {
-            scene.config.global.minval = value;
-            scene.updateConfig();
-        });
-
-        gui.maxval = 100000;
-        gui.add(gui, 'maxval', 0, 100000).name("maximum value").onChange(function(value) {
-            scene.config.global.maxval = value;
-            scene.updateConfig();
-        });
-
-        gui.divisions = 6;
-        gui.add(gui, 'divisions', 3, 9).listen().name("divisions").onChange(function(value) {
-            gui.divisions = Math.round(value);
-            scene.config.global.divisions = Math.round(value);
-            scene.updateConfig();
-        });
     }
     /***** Render loop *****/
 
